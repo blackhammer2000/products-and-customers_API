efc410ec6f55ffe68a5c14d8d1ef79e9a6d40f4d
wawerusamuel <waweruzamuel@gmail.com>
2022-11-01 17:00:07 +0300

    created an access token generator function, now working on the function that verifies the token...

--- auth/tokens/create_token.js
+++ auth/tokens/create_token.js
@@ -1,8 +1,25 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const signAccessToken = (_id) => {
+const signAccessToken = (customerID) => {
   return new Promise((resolve, reject) => {
-    const token = jwt.sign({ _id }, process.env.SECRET_KEY);
+    const token = jwt.sign({ customerID }, process.env.SECRET_KEY);
+
+    if (!token) reject(token);
+
+    resolve(token);
   });
+};
+
+const verifyAccessToken = async (req, res, next) => {
+  try {
+    if (!req.headers.token) throw new Error("Unauthorized");
+    const { customerID } = await jwt.verify(
+      req.headers.token,
+      process.env.SECRET_KEY,
+      options
+    );
+  } catch (err) {
+    res.json({ error: err.message });
+  }
 };
