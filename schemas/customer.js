5243064991a9054790432178a8d22e6a3a84419a
wawerusamuel <waweruzamuel@gmail.com>
2022-11-01 15:44:42 +0300

    created a customer schema model using mongoose...

---
+++ schemas/customer.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const customerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Customer = model("customer", customerSchema);
+
+module.exports = Customer;
