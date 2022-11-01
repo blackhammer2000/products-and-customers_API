c31efaa38722698f1318ba3ac03a76280935ed50
wawerusamuel <waweruzamuel@gmail.com>
2022-11-01 15:50:43 +0300

    created a schema for the products to be added to the db...

---
+++ schemas/product.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Product = model("product", productSchema);
+
+module.exports = Product;
